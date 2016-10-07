2a2ab00970bc9cde768df0dbc976f3a6415d25ee
Spanfile <santeri.kasvi@gmail.com>
2016-10-07 18:03:47 +0300

    Debug printing tree elements

--- FunctionCalculator/main.cpp
+++ FunctionCalculator/main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 
 void free_elem(TREE_ELEMENT*);
+void print_elem(TREE_ELEMENT*, int);
 
 int main(void)
 {
@@ -33,6 +34,7 @@ int main(void)
     int index = 0;
     TREE_ELEMENT* root_elem = parse(tokens, token_count, &index);
 
+    print_elem(root_elem, 0);
     free_elem(root_elem);
 
     for (int i = 0; i < (int)token_count; i++)
@@ -54,4 +56,33 @@ void free_elem(TREE_ELEMENT* elem)
         free_elem(elem->child2);
 
     free(elem);
+}
+
+void print_elem(TREE_ELEMENT* elem, int indent)
+{
+    for (int i = 0; i < indent; i++)
+        printf(" ");
+
+    printf("%d: ", elem->type);
+
+    switch (elem->type)
+    {
+    case TYPE_NUMBER:
+        printf("%f\n", elem->number_value);
+        break;
+
+    case TYPE_NAME:
+        printf("%s\n", elem->name_value);
+        break;
+
+    case TYPE_ARITHMETIC:
+        printf("%d\n", elem->arithmetic_type);
+
+        if (elem->child1 != nullptr)
+            print_elem(elem->child1, indent + 1);
+
+        if (elem->child2 != nullptr)
+            print_elem(elem->child2, indent + 1);
+        break;
+    }
 }
